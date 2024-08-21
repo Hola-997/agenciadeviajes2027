c805d24d129864ec111cf95b2e19b10b3df16213
Hola-997 <saske7626@gmail.com>
2024-08-21 16:54:55 -0500

    Inicializando proyecto Agenciadeviajes2027

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>(Agencia de viajes)</title>
+        @vite('resources/css/app.css') 
+    </head>
+    <body>
+        <header>
+
+            {{-- barra de navegación --}}
+            @include('layouts.navbar')
+
+        </header>
+
+        <main>
+
+            @yield('contenido',)
+
+        </main>
+
+        <footer>
+
+            @include('layouts.footer')
+
+        </footer>
+
+    </body>
+    </html>
